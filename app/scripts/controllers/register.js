165c4f58be2df0a729bcb6211882dcaf527bab8c
Dave Harned <dharned@gmail.com>
2017-04-15 11:17:19 -0500

    setup post request

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('projectApp')
-  .controller('RegisterCtrl', function ($scope) {
+  .controller('RegisterCtrl', function ($scope, $http) {
     $scope.submit = function() {
       console.log('submit triggered!');
+      console.log($scope.register.email.$viewValue);
+      var url = '/';
+      var user = {
+        email: $scope.register.email.$viewValue,
+        password: $scope.register.password.$viewValue
+      };
+
+      $http.post(url, user)
+        .success(function(res) {
+          console.log('success');
+        })
+        .error(function(err) {
+          console.log('error');
+        })
     };
   });
